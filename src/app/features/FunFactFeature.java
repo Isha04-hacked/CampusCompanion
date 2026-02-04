29d62380226ad291ce08ccfcfc771241a2b38410
isha patel <patelishur154@gmail.com>
2026-02-03 21:08:30 -0500

    Add/update fun-fact content in FunFactFeature.java

--- src/app/features/FunFactFeature.java
+++ src/app/features/FunFactFeature.java
@@ -4,9 +4,9 @@ public class FunFactFeature {
     public static String run() {
         return "Fun Fact\n"
              + "------------------------------\n"
-             + "Student Name: YOUR NAME HERE\n"
-             + "Fun Fact About You:\n"
-             + "Replace this text with a fun fact.\n"
+             + "Student Name: Isha Patel\n"
+             + "Did you know? Video games can improve problem-solving and reaction time.\n"
+             + "I love playing and designing games\n"
              + "------------------------------\n";
     }
 }
